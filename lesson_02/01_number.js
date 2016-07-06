0d70b74fa7f61a6cf61dc4be5f55cb46ba826c20
Son Ha <hason61vn@gmail.com>
2016-07-06 21:30:45 +0700

    add new

---
+++ lesson_02/01_number.js
@@ -0,0 +1,19 @@
+console.log(1+2);
+var a = 1 - 0.3 + 0.1;
+console.log(a);
+
+var c = 5/0;
+console.log(c);
+
+var d = -5/0;
+console.log(d);
+
+//parse qua kieu int
+var e = '10';
+console.log(parseInt(e));
+
+//Not a Number
+var f = 'babba';
+console.log(parseInt(f));
+console.log(!f);
+console.log(isFinite(d));
---
+++ lesson_02/02_string.js
@@ -0,0 +1,18 @@
+var string =  'dfdfdf';
+console.log(string.length);
+
+console.log(8 + 'babaa');
+console.log(8 + 3 + "babaa");
+console.log('ressult is ' +8 + 3);
+
+console.log("hello".indexOf('l'));
+console.log("Hello World".substring(6,5));
+console.log("Hello World".slice(6,11));
+
+var a = "Hello World".split(" ");
+
+var a_str = a.join(" ");
+
+console.log(a_str);
+
+console.log(a.trim);
