bed79c50b1681b0a5db81644a704a8079ccc591b
Son Ha Anh <Son Ha Anh>
2016-07-07 14:50:14 +0700

    add new

--- lesson_02/02_string.js
+++ lesson_02/02_string.js
@@ -6,11 +6,11 @@ console.log(3 + "dung");
 console.log('ressult is ' +8 + 3);
 
 console.log("hello".indexOf('l'));
-console.log("Hello World".substring(6,5));
-console.log("Hello World".slice(6,11));
+console.log("Son World".substring(6,5));
+console.log("Hello World".slice(6,8));
 
 var a = "Hello World".split(" ");
-
+console.log(a[0]);
 var a_str = a.join(" ");
 
 console.log(a_str);
