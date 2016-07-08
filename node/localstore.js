53c8a1e56da197fc9555b9a5a9b121d10b381852
= <=>
2016-07-08 09:13:51 +0700

    update localstore

--- node/localstore.js
+++ node/localstore.js
@@ -1,24 +1,65 @@
 var storage = require ('node-persist');
 
-storage.initSync({dir:'stutents'});
+storage.initSync({
+	dir:'students',
+	ttl:false
+});
 
-function getAllStutents(){
-	var students = stogare.getItemSync('students');
+function getAllStuDents(){
+	var students = storage.getItemSync('students');
 	if (typeof students === 'undefined'){
-		return Null;
-	} else return stutents;
+		return [];
+	} else return students;
 }
 
-function getDetailStudents(){
+function getDetailStudents(s_id){
+	var students = getAllStuDents();
+
+	return students[s_id];
+}
+function addNewStudent(name,birthday,email){
+
+	var students = getAllStuDents();
+	
+	var new_student = {};
+	new_student.id = students.length + 1;
+	new_student.name = name;
+	new_student.birthday = birthday;
+	new_student.email = email;
+
+	students.push(new_student);
+
+	storage.setItemSync("students",students);
+}
+function deleteStuden(s_id){
+	var students = getAllStuDents();
+
+	for (var i = 0;i < students.length ;i ++){
+		if (students[i].id === s_id){
+			storage.removeItemSync(students[i]);
+			return 'Done';	
+		} 
+	}
+}
+function editstudent(s_id,name,birthday,email){
+	var students = getAllStuDents();
+
+	students[s_id].id = s_id;
+	students[s_id].name = name;
+	students[s_id].birthday = birthday;
+	students[s_id].email = email;
+
+	storage.setItemSync('students',students);
+	return 'Done';
+
 
 }
-function addNewStudent(){
 
-}
-function deleteStuden(){
+addNewStudent('Duc Linh','17/08/1989','haanhduclinh@gmail.com');
+addNewStudent('Duc Linh','17/08/1989','haanhduclinh@gmail.com');
+addNewStudent('Duc Linh','17/08/1989','haanhduclinh@gmail.com');
+addNewStudent('Duc Linh','17/08/1989','haanhduclinh@gmail.com');
+addNewStudent('Duc Linh','17/08/1989','haanhduclinh@gmail.com');
 
-}
-function editstudent(){
-
-}
-//cat ~/.ssh/id_rsa.pub
+var result = getAllStuDents();
+console.log(result);
