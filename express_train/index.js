8ccff52089c673b014aaad9d398738879214534d
Simon Ha <hason61vn@gmail.com>
2016-07-15 16:23:58 +0700

    edit code

--- express_train/index.js
+++ express_train/index.js
@@ -24,12 +24,6 @@ app.post('/', function (req, res) {
    res.send('Hello POST');
 })
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
-
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
    console.log("Got a DELETE request for /del_user");
@@ -60,7 +54,7 @@ app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
    response = {
        name:req.query.name,
-       age:req.query.age,
+       age:req.query.age,;
        address:req.query.address
    };
    console.log(response);
