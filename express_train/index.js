5ddc93c734596366667ef306d685ed8d542d129f
Son Ha <hason61vn@gmail.com>
2016-07-18 19:38:53 +0700

    add new

--- express_train/index.js
+++ express_train/index.js
@@ -2,18 +2,29 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));// Muon lay anh o thu muc nao thi khai bao o day
 // app.use(express.static('public/images')); Neu doi thanh nhu nay thi url se la : http://localhost:3000/01.jpg 
 // Neu dat
 
 // Basic routing
-app.get('/', function(req, res){
-	res.send("This is first application");
+app.get('/', function (req, res){
+  res.sendFile( __dirname + "/" + "index.html");
 });
 
-app.get('/demo', function(req, res) {
+app.post('/', function (req, res) {
+   // Prepare output in JSON format
+    response = {
+       name:req.query.name,
+       age:req.query.age,
+       address:req.query.address
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+app.get('/demo', function (req, res) {
 	res.send("This is demo page");
 });
 
@@ -21,32 +32,32 @@ app.get('/demo', function(req, res) {
 app.post('/', function (req, res) {
    console.log("Got a POST request for the homepage");
    res.send('Hello POST');
-})
+});
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
    console.log("Got a DELETE request for /del_user");
    res.send('Hello DELETE');
-})
+});
 
 // This responds a GET request for the /list_user page.
 app.get('/list_user', function (req, res) {
    console.log("Got a GET request for /list_user");
    res.send('Page Listing');
-})
+});
 
 // This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
+app.get('/ab*cd', function (req, res) {   
    console.log("Got a GET request for /ab*cd");
    res.send('Page Pattern Match');
-})
+});
 
 app.get('/formGet.html', function (req, res) {
    res.sendFile( __dirname + "/" + "formGet.html" );
-})
+});
 
 app.get('/formPost.html', function (req, res) {
    res.sendFile( __dirname + "/" + "formPost.html" );
-})
+});
 
 app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
@@ -57,7 +68,7 @@ app.get('/process_get', function (req, res) {
    };
    console.log(response);
    res.end(JSON.stringify(response));
-})
+});
 
 app.post('/process_post', function (req, res) {
    // Prepare output in JSON format
@@ -68,7 +79,7 @@ app.post('/process_post', function (req, res) {
    };
    console.log(response);
    res.end(JSON.stringify(response));
-})
+});
 
 
 
