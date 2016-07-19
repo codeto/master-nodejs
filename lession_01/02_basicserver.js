ffaa70af53cd2fd1be5d61e29156009ba0d7f182
Son Ha <hason61vn@gmail.com>
2016-07-19 23:46:43 +0700

    error

--- lession_01/02_basicserver.js
+++ lession_01/02_basicserver.js
@@ -15,7 +15,7 @@ var server = http.createServer(function(req, res){
     // res.writeHead(200, {"Content-Type": "text/plain"});
     res.writeHead(200, {"Content-Type": "application/json"});
     // res.write("Hello, I am basic server NodeJS");
-    res.write(JSON.stringify(result));1
+    res.write(JSON.stringify(result));
     res.end();
 });
 
